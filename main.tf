6eb08fef0a3231ec77ce970a4ae8d94a6cbe474b
DhanushGoudAngara <103447259+DhanushGoudAngara@users.noreply.github.com>
2024-08-01 13:13:18 +0530

    Update main.tf
    compute Engine

--- main.tf
+++ main.tf
@@ -3,10 +3,49 @@ provider "google" {
   region      = "us-central1"
   zone        = "us-central1-c"
 }
+resource "google_service_account" "default" {
+  account_id   = "my-custom-sa"
+  display_name = "Custom SA for VM Instance"
+}
 
-resource "google_compute_network" "vpc_network" {
-  project                 = "My First Project"
-  name                    = "vpc-network-1"
-  auto_create_subnetworks = true
-  mtu                     = 1460
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "n2-standard-2"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
 }
